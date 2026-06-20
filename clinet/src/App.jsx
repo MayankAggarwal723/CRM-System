c02b38e7249ccd01a07ea0d3ee5cfbf0605e9a6f
Mayank Aggarwal <147256298+MayankAggarwal723@users.noreply.github.com>
2026-06-20 16:13:19 +0530

    leads

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import AdminDashboard from "./pages/admin/Dashboard";
-import EmployeeDashboard from "./pages/employees/EmployeeDashboard";
 import Employee from "./pages/admin/Employee";
 import Lead from "./pages/admin/Lead";
+import EmployeeDashboard from "./pages/employees/EmployeeDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/employee" element={<Employee />} />
-        <Route path="/employees/dashboard" element={<EmployeeDashboard />} />
         <Route path="/admin/lead" element={<Lead />} />
+
+        <Route path="/employees/dashboard" element={<EmployeeDashboard />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
